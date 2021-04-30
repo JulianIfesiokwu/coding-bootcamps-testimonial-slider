86c08abf68674488c394f0c8fd79e4c6a3bf3cfd
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-04-30 10:29:42 +0100

    completed functionality using javascript, working on background svg

---
+++ scripts/main.js
@@ -0,0 +1,57 @@
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const clientQuote = document.querySelector('.client-quote')
+const imgHolder = document.querySelector('img');
+const clientName = document.querySelector('.client-name');
+const occupation = document.querySelector('.client-occupation');
+
+const clients = [
+    {
+        name: 'Tanya Sinclair',
+        occupation: 'UX Engineer',
+        src: './assets/image-tanya.jpg',
+        quote: `“ I’ve been interested in coding for a while but never taken the jump, until now. 
+        I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+        excited about the future. ”`
+    },
+    {
+        name: 'John Tarkpor',
+        occupation: 'Junior Front-end Developer',
+        src: './assets/image-john.jpg',
+        quote: `“ If you want to lay the best foundation possible I’d recommend taking this course. 
+        The depth the instructors go into is incredible. I now feel so confident about 
+        starting up as a professional developer. ”`
+    }
+]
+
+// functions
+function prevClient(e) {
+    let clientIndex = 0;
+    let currentClientName = clientName.textContent;
+
+    clientQuote.textContent = clients[clientIndex].quote;
+    imgHolder.setAttribute('src', clients[clientIndex].src);
+    clientName.textContent = clients[clientIndex].name;
+    occupation.textContent = clients[clientIndex].occupation;
+
+    if(clientName.textContent === currentClientName) return;
+    if(clientName.textContent !== currentClientName) clientIndex-1
+}
+
+
+function nextClient(e) {
+    let clientIndex = 1;
+    let currentClientName = clientName.textContent;
+
+    clientQuote.textContent = clients[clientIndex].quote;
+    imgHolder.setAttribute('src', clients[clientIndex].src);
+    clientName.textContent = clients[clientIndex].name;
+    occupation.textContent = clients[clientIndex].occupation;
+
+    if(clientName.textContent === currentClientName) return;
+    if(clientName.textContent !== currentClientName) clientIndex+1
+}
+
+// event listeners
+prevBtn.addEventListener('click', prevClient);
+nextBtn.addEventListener('click', nextClient);
